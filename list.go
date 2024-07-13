b29be7ede98f98208cf3bc2daaf038e0608a5cd2
Dušan Simić <dusan.simic1810@gmail.com>
2024-07-13 20:18:07 +0200

    fix: Completions

--- list.go
+++ list.go
@@ -2,24 +2,17 @@ package shanghai
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/dusansimic/shanghai/file"
-	"gopkg.in/yaml.v3"
 )
 
 func ListImages(f string) ([]string, error) {
-	b, err := os.ReadFile(f)
+	fileStruct, err := file.Read(f)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 
-	var file file.File
-	if err := yaml.Unmarshal(b, &file); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal file: %w", err)
-	}
-
-	is := file.Tree.Nodes()
+	is := fileStruct.Tree.Nodes()
 	keys := make([]string, len(is))
 	for _, im := range is {
 		keys = append(keys, im.Name())
